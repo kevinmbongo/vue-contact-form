0445dc0f8a2770f7d646a2ee2635bf6816ead92e
kevinm <mbongo.kevin.pro@gmail.com>
2022-01-05 21:02:26 +0100

    feat(project): connect all inputs to the clobal state

---
+++ src/component/VTextarea.vue
@@ -0,0 +1,35 @@
+<template>
+  <textarea
+    rows="7"
+    :value="value"
+    placeholder="Message *"
+    @input="handleInput"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'VTextarea',
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e.target.value)
+    },
+  },
+}
+</script>
+
+<style scoped>
+textarea {
+  @apply border border-blue rounded w-full px-3;
+}
+::placeholder {
+  @apply text-blue text-base;
+}
+</style>
